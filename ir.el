d27aac43a18813e947fbf63cb80a8f567416a3f4
adham-omran <adham.rasoul@hotmail.com>
2022-08-10 12:36:55 +0300

    Bump version to 0.7.0

--- ir.el
+++ ir.el
@@ -6,7 +6,7 @@
 ;; Maintainer: Adham Omran <adham.rasoul@gmail.com>
 ;; Created: June 22, 2022
 ;; Modified: June 22, 2022
-;; Version: 0.6.0
+;; Version: 0.7.0
 ;; Keywords: wp, incremental reading
 ;; Homepage: https://github.com/adham-omran/ir
 ;; Package-Requires: ((emacs "24.4"))
